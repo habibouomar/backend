ed46191c9408a1491c6e961f1b478cfa6f731309
habibouomar <habibou.omar@gmail.com>
2022-04-13 15:01:13 +0200

    form creation and testing

--- chapitre_2/formulaire/index.js
+++ chapitre_2/formulaire/index.js
@@ -5,7 +5,21 @@ const engine = require("express-handlebars").engine
 app.engine("handlebars", engine())
 app.set('view engine', 'handlebars')
 
+app.use(express.urlencoded({ extended: true }))
+
 const port = 3000;
 app.listen(port, () => {
     console.log('Server started on port: ' + port);
+});
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
+
+app.post('/signup', (req, res) => {
+    console.log(req.body.username) 
+    console.log(req.body.password) 
+    res.render('signup',{
+        username: req.body.username 
+    });
 });
